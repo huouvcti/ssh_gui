6d27a03cc46b47a533c2247b91ae03a0491eff8a
huouvcti <huouvcti@gmail.com>
2023-11-16 12:15:46 +0900

    update

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+
+router.get('/GUI', function(req, res, next) {
+  res.render('GUI');
+});
+
 module.exports = router;
--- routes/ssh.js
+++ routes/ssh.js
@@ -1,54 +1,69 @@
 var express = require('express');
-const { Client } = require('ssh2');
-
 var router = express.Router();
+const SSH = require('ssh2').Client;
+
+
+// const sshConfig = {
+//     host: '103.60.127.85',
+//     port: 22,
+//     username: 'root',
+//     password: 'o2webserver~', // 또는 키파일 사용 가능
+// };
+
+const sshConfig = {
+  host: "",
+  port: "",
+  username: "",
+  password: "",
+}
+
+
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-    // const sshConfig = {
-    //     host: '103.60.127.85',
-    //     port: 22,
-    //     username: 'root',
-    //     password: 'o2webserver~', // 또는 키파일 사용 가능
-    // };
-
-
-    const sshConfig = {
-        host: req.query.host,
-        port: req.query.port,
-        username: req.query.username,
-        password: req.query.pw,
-    }
+router.post('/config', function(req, res, next) {
+    sshConfig.host = req.body.host;
+    sshConfig.port = req.body.port;
+    sshConfig.username = req.body.username;
+    sshConfig.password = req.body.pw;
     
-      const command = 'ls'; // 실행할 SSH 명령
-    
-      const conn = new Client();
-      conn.on('ready', () => {
-        conn.exec(command, (err, stream) => {
-          if (err) {
-            res.send(err)
-          }
-
-          let result = [];
-    
-          stream
-            .on('data', (data) => {
-                const lsList = data.toString('utf8').split("\n")
-
-                lsList.map((value) => {
-                    if(value){
-                        result.push(value);
-                    }
-                    
-                })
-              
-            })
-            .on('close', (code, signal) => {
-              conn.end();
-              res.send(result); // 결과를 클라이언트로 전송
-            });
-        });
-      }).connect(sshConfig);
+    res.send("<script>location.href='/GUI';</script>");
 });
+
+
+router.get('/start', function(req, res, next) {
+  const conn = new SSH();
+  
+  const command = 'ls ~'; // 실행할 SSH 명령
+  
+  conn.on('ready', () => {
+    conn.exec(command, (err, stream) => {
+      if (err) {
+        res.send(err)
+      }
+
+      let result = [];
+
+      stream
+        .on('data', (data) => {
+          const lsList = data.toString('utf8').split("\n")
+
+          lsList.map((value) => {
+              if(value){
+                  result.push(value);
+              }
+              
+          })
+        })
+        .on('close', (code, signal) => {
+          conn.end();
+          res.send(result);
+        });
+    });
+  }).connect(sshConfig);
+});
+
+
+
+
 
 module.exports = router;
